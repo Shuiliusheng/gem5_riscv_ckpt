f35c39510c27431a7584a6c2d13890c2adc69310
Shuiliusheng <cuihongwei@pku.edu.cn>
2023-11-04 19:46:09 +0800

    modified the arch/riscv codes to support temp registers for running readckpt.riscv

--- createCkpt/gem5_create/arch/riscv/decoder.cc
+++ createCkpt/gem5_create/arch/riscv/decoder.cc
@@ -81,9 +81,12 @@ Decoder::moreBytes(const PCState &pc, Addr fetchPC)
     }
 }
 
+#define Support_ReadCkpt_TempRegs 0
+
 StaticInstPtr
 Decoder::decode(ExtMachInst mach_inst, Addr addr)
 {
+#ifndef Support_ReadCkpt_TempRegs
     DPRINTF(Decode, "Decoding instruction 0x%08x at address %#x\n",
             mach_inst, addr);
 
@@ -96,6 +99,67 @@ Decoder::decode(ExtMachInst mach_inst, Addr addr)
     DPRINTF(Decode, "Decode: Decoded %s instruction: %#x\n",
             si->getName(), mach_inst);
     return si;
+
+#else
+    //new imm[11:0] rs1 000 rd 0010011
+    //由readckpt中对临时寄存器的支持指令决定
+    //addi x0, rs1, #9+: write temp,     op = 0x13, func3 = 0, 
+    //addi x0, rs1, #36+: read temp,   op = 0x13, func3 = 0, 
+    //addi x0, rs1, #64: jmp rtemp,    op = 0x13, func3 = 0, 
+
+    unsigned int taddinst = 0x80b3; //add rd (1), rs1(1), rs2(0)
+    unsigned int tjalrinst = 0x8067; //jalr rd(0), rs1(1), 0
+    ExtMachInst old_mach_inst = mach_inst;
+
+    unsigned int opcode = 0, rs1 = 0, rs2 = 0, rd = 1, imm12 = 0, func3 = 0;
+    bool isRTemp = false, isWTemp = false, isJTemp = false;
+    if (!compressed(emi)) {
+        opcode = mach_inst % 128;
+        imm12 = (mach_inst >> 20);
+        func3 = (mach_inst >> 12) % 8;
+        rs1 = (mach_inst >> 15) % 32;
+        rs2 = (mach_inst >> 20) % 4;
+        rd = (mach_inst >> 7) % 32;
+
+        bool isNewInst = (opcode == 0x13) && (func3 == 0) && (rd == 0);
+
+        isWTemp = isNewInst && (imm12 >= 9 && imm12 <= 12);
+        isRTemp = isNewInst && (imm12 >= 36 && imm12 <= 39);
+        isJTemp = isNewInst && (imm12 == 64);
+    }
+    
+    if(isRTemp || isWTemp){
+        mach_inst = taddinst;
+    }
+    else if(isJTemp){
+        mach_inst = tjalrinst;
+    }
+
+    StaticInstPtr &si = instMap[mach_inst];
+    if (!si || isRTemp || isWTemp || isJTemp){ //如果是临时指令，则不从cache中取，而是直接重新译码
+        si = decodeInst(mach_inst);
+    }  
+
+    if(isRTemp) {
+        si->setSrcRegIdx(0, RegId(IntRegClass, 32+imm12-36));
+        si->setDestRegIdx(0, RegId(IntRegClass, rs1));
+        //reg[rs1] = reg[rtemp(imm12-36)] + 0
+    }
+    else if(isWTemp) {
+        si->setSrcRegIdx(0, RegId(IntRegClass, rs1));
+        si->setDestRegIdx(0, RegId(IntRegClass, 32+imm12-9));
+        //reg[rtemp(imm12-36)] = reg[rs1] + 0
+    }
+    else if(isJTemp) {
+        si->setSrcRegIdx(0, RegId(IntRegClass, 32+rs1%4));
+        //jalr x0, 0(reg[rtemp(32+rs1%4)])
+    }
+
+    DPRINTF(Decode, "Decode: Decoded %s instruction: %#x\n",
+            si->getName(), mach_inst);
+    return si;
+
+#endif
 }
 
 StaticInstPtr
